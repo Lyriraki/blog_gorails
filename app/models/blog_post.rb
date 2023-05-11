96d1c9104c5de7e17943020f612987e39ae51d26
lyriraki <luckyarif21@gmail.com>
2023-05-12 01:35:06 +0700

    Adding new query for short blog

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,7 +3,14 @@ class BlogPost < ApplicationRecord
   validates :body, presence: true
 
   # TODO: Pelajari lagi code lambda di bawah ini
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  # scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  # ! Intinya coding di atas tidak berjalan sebagaimana mestinya
+  # ! Karena harusnya urutannya scheduled-publish-draft tapi coding di atas malah tidak berjalan
+  # * Maka dari itu ada 2 opsi untuk melakukan query
+  # * Pertama - Pakai raw Query dari PostgreSQL
+  # * scope :sorted, -> { order(Arel.sql('published_at DESC NULLS LAST')).order(updated_at: :desc) }
+  # * Kedua - Pakai Query Ruby
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc) }
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where('published_at <= ?', Time.current) }
   scope :scheduled, -> { where('published_at > ?', Time.current) }
